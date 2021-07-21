b2cb3186df9bda14ce5caf0d5812846566ef9cd6
Aryan <test@test.com>
2021-07-21 17:51:27 +0530

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const { registerUser } = require('../controllers/users');
+
+
+router.route('/').post(
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Invalid email').isEmail(),
+    check('password', 'Password must be atleast 6 characters').isLength({
+      min: 6
+    })
+  ],
+  registerUser
+);
+
+module.exports = router;
