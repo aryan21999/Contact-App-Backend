987acca643d44cb8c415df86fefbd948f3e8f7cb
Aryan <test@test.com>
2021-07-23 13:01:37 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-//connect to DataBase
 connectDB();
 
-//middleware
 app.use(express.json({ extended: false }));
 
-//Define routes
 app.use('/api/users', require('./routes/users'));
-// app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-//serve static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
